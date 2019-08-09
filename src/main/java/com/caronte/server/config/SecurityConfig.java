c27af3476d732862d40be3c1f227528e533426fd
Caio Cesar <caio.crd@gmail.com>
2019-08-09 10:53:14 -0300

    Configuracoes de producao

--- src/main/java/com/caronte/server/config/SecurityConfig.java
+++ src/main/java/com/caronte/server/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                         .permitAll()
-                    .antMatchers("/auth/**")
+                    .antMatchers("/**")
                         .permitAll()
                     .antMatchers("/user/checkUsernameAvailability", "/user/checkEmailAvailability")
                         .permitAll()
